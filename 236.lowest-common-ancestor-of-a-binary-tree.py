598e910acd898c44e8f1f5873e3ca9d322cd0a1a
zhangqiang <tyokyo320@gmail.com>
2020-12-22 00:59:40 +0900

    leetcode 236

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -13,7 +13,57 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
+        path = []
+        node_p_path = []
+        node_q_path = []
+        finish = 0
+        
+        self.preorder(root, p, path, node_p_path, finish)
+        # print(node_p_path[0])
+        
+        # 清空path，finish，计算q节点路径
+        path.clear()
+        finish = 0
+        self.preorder(root, q, path, node_q_path, finish)
+
+        #print(node_q_path[0])
+        lp = node_p_path[0]
+        lq = node_q_path[0]
+
+        if len(lp) < len(lq):
+            path_len = len(lp)
+        else:
+            path_len = len(lq)
+        # print(path_len)
+
+        result = TreeNode(0)
+        for i in range(path_len):
+            if lp[i] == lq[i]:
+                result = lp[i]
+
+        return result
+
+    
+    def preorder(self, node, search, path, result, finish):
+        # finish未找到为0，找到为1
+        if (node is None) or finish:
+            return
+        
+        path.append(node)
+        if node == search:
+            finish = 1
+            result.append(list(path))
+            result = result[0]
+            # print(result)
+        
+        self.preorder(node.left, search, path, result, finish)
+        self.preorder(node.right, search, path, result, finish)
+
+        path.pop()
+
+
+
         
 # @lc code=end
 
