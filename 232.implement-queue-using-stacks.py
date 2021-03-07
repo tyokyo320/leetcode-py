b3f761412726696d63773919043a8f7721e2bb16
zhangqiang <tyokyo320@gmail.com>
2021-03-07 21:15:06 +0900

    queue 232

--- 232.implement-queue-using-stacks.py
+++ 232.implement-queue-using-stacks.py
@@ -11,30 +11,42 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
+        self.stack_enqueue = []
+        self.stack_dequeue = []
         
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
+        self.stack_enqueue.append(x)
         
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
+        if not self.stack_dequeue:
+            while self.stack_enqueue:
+                self.stack_dequeue.append(self.stack_enqueue.pop())
+        return self.stack_dequeue.pop()
         
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if not self.stack_dequeue:
+            while self.stack_enqueue:
+                self.stack_dequeue.append(self.stack_enqueue.pop())
+        return self.stack_dequeue[-1]
         
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
+        return not self.stack_enqueue and not self.stack_dequeue
         
 
 
