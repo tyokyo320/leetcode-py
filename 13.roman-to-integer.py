653b6247a393d0bd97ab3d7c590ca0aaf23176b1
zhangqiang <tyokyo320@gmail.com>
2021-03-14 22:28:17 +0900

    string 13

---
+++ 13.roman-to-integer.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000,
+        }
+
+        result = 0
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC')
+
+        for char in s:
+            result += roman[char]
+        
+        return result
+
+# @lc code=end
+
