4ab8270b4dd3ba394d4d20c86e90b3a0b21debb5
zhangqiang <tyokyo320@gmail.com>
2021-03-17 23:25:28 +0900

    added sort algorithms

---
+++ sort/insertion_sort.py
@@ -0,0 +1,44 @@
+
+class SortAlgorithms():
+
+    def insertionSort(self, array):
+        for i in range(1, len(array)):
+            # 选未排序的第一个值 value
+            value = array[i]
+            # 从 value 的前一个位置开始比较
+            position = i - 1
+
+            # 若 value 比前一个位置的值小
+            while position >= 0 and array[position] > value:
+                # 用前一个元素覆盖当前元素
+                array[position + 1] = array[position]
+                position -= 1
+
+            # 循环结束后，将 value 插入到相应位置
+            array[position + 1] = value
+        return array
+
+    def selectionSort(self, array):
+        # 最后一轮不需要遍历，已经有顺序了
+        for i in range(len(array) - 1):
+            min_index = i
+            # 循环查找最小值
+            for j in range(i + 1, len(array)):
+                if array[min_index] > array[j]:
+                    min_index = j
+            # 交换
+            array[min_index], array[i] = array[i], array[min_index]
+
+        return array
+
+
+if __name__ == "__main__":
+    s = SortAlgorithms()
+    unsorted_array = [64, 34, 25, 12, 22, 11, 90]
+
+    # 插入排序
+    # array = s.insertionSort(unsorted_array)
+
+    # 选择排序
+    array = s.selectionSort(unsorted_array)
+    print(array)
