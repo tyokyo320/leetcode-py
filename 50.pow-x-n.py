e32a6a6e715ba7f181c386042c18ff6ae078557c
zhangqiang <tyokyo320@gmail.com>
2021-04-04 22:07:00 +0900

    math 50

---
+++ 50.pow-x-n.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=50 lang=python3
+#
+# [50] Pow(x, n)
+#
+
+# @lc code=start
+
+import math
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return math.pow(x, n)
+        
+# @lc code=end
+
