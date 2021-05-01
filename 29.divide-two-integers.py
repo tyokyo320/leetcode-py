0d8208937596e51fc89c4097335bd8385c1648f8
zhangqiang-xps <tyokyo320@gmail.com>
2021-05-01 22:33:40 +0900

    math 29

---
+++ 29.divide-two-integers.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=29 lang=python3
+#
+# [29] Divide Two Integers
+#
+
+# @lc code=start
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        else:
+            return int(dividend / divisor)
+# @lc code=end
+
