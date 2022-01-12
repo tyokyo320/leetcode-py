b75100b2297afbc1f773e29dfb69c11627d07cab
zhangqiang <tyokyo320@gmail.com>
2022-01-12 21:24:08 +0900

    ✨ feat: add golang code

---
+++ 35.search-insert-position.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=35 lang=golang
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+
+	len := len(nums)
+
+	start := 0
+	end := len - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+		if nums[mid] < target {
+			start = mid + 1
+		} else if nums[mid] > target {
+			end = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return start
+}
+
+// @lc code=end
+
